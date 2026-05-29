f676cdc1c0ef016393f1d0e91d915f6da55f07c1
Marc0-7 <marcoelyas77@gmail.com>
2026-05-29 12:44:58 +0200

    Create package-info.java

---
+++ memoria/package-info.java
@@ -0,0 +1,8 @@
+package memoria;
+
+/*
+	Memoria di archiviazione di calcoli che poi verranno recuperati
+	Un semplice file di testo che viene manipolato in modo da avere 10 righe come dimensione massima
+	E' possibile aggiungere altri file per aggiungere più memorie quando si estenderanno le modalità
+
+*/
