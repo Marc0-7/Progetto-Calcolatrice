5d56c65a3d44c2b28c11c0c87592a8b4065d2b07
Marc0-7 <marcoelyas77@gmail.com>
2026-05-29 12:41:22 +0200

    Create package-info.java

---
+++ calcolatrice/package-info.java
@@ -0,0 +1,14 @@
+package calcolatrice;
+
+/*
+	1) 
+	
+	Probabilmente il package più complesso del progetto
+	Per ora solo due classi.
+	- Window, per la finestra grafica
+	- Funzioni, per metodi relativi a calcoli o codici utili che possono essere riutilizzati
+	
+	2) 
+	Iniziato a sviluppare i menu e sono da finire la parte riguardante la memoria e cronologia
+
+*/
